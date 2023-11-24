2ef08b370c0373bac01452ea687496d1759d9ff0
Nelson Rivera <isaacriverahn7@gmail.com>
2023-11-24 11:24:02 -0600

    Add HTML media player content, ng-template and ng-container

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.css']
+  styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent {
-
+  mockCover: any = {
+    cover:
+      'https://i0.wp.com/edmjoy.com/wp-content/uploads/3052fa49-7a5a-1f3e-57b6-cf778c50300a.jpg?w=1200&ssl=1',
+    name: 'Gioli & Assi',
+    title: 'BEBE (Oficial)',
+  };
 }
